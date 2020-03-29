9e76f05098d0fb22ae655aa334d0f6fa242eb1f0
destris01 <46545841+destris01@users.noreply.github.com>
2020-03-29 17:45:40 +0200

    final-version

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("test")
+const scroll = new SmoothScroll('.section-hero a[href*="#smooth"]', {
+    speed: 800
+});
+
+const scroll2 = new SmoothScroll('.intro a[href*="#history-section"]', {
+    speed: 800
+});
